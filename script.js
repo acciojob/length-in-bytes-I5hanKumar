de977e415fd70e467d857a596857f424cfd68117
I5hanKumar <153290701+I5hanKumar@users.noreply.github.com>
2025-09-03 22:39:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 const byteSize = (str) => {
   // write your code here
-	if(null===str){
+	if(null===str || str.length===0){
     return 0
- }
-	return str.length
+}
+
+let ans=0;
+
+  for (let i = 0; i < str.length; i++) {
+    // Check if any character's code is outside the ASCII range (0-127)
+    if (str.charCodeAt(i) > 127) {
+		
+		
+      ans=ans+3;
+		
+		
+    }
+	else{
+   ans=ans+1;   
+  }  
+  }
+   
+return ans;
 	
 };
 
